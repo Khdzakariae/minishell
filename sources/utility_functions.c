4356732167741852d4dc4342cd37418335549e31
Zakariae El-Khadir <zel-khad@e2r10p2.1337.ma>
2024-06-08 19:09:02 +0100

    Refactor: Implement ft_strcpy and fix add_node to handle strings
    This commit refactors the codebase by implementing the ft_strcpy function and correcting the add_node function to handle strings correctly. These changes improve the code's functionality and maintainability.

--- sources/utility_functions.c
+++ sources/utility_functions.c
@@ -1,5 +1,23 @@
 #include <minishell.h>
 
+char *ft_strcpy(char *src, char *dst)
+{
+    if (src == NULL || dst == NULL) 
+	{
+        return NULL;
+    }
+
+    size_t i = 0;
+
+    while (src[i]) 
+	{
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return dst;
+}
+
  void freeList(t_token* head) 
  {
     t_token* current = head;
@@ -13,13 +31,14 @@
 	free(current);
 }
 
-void	add_node(t_token **head, char symbol)
+void	add_node(t_token **head, char *symbol)
 {
 	t_token	*tmp;
 	t_token	*temp1;
 
 	tmp = malloc(sizeof(t_token));
-	tmp->type = symbol;
+	ft_strcpy(symbol, symbol);
+	symbol = symbol;
 	tmp->next = NULL;
 	if (*head == NULL)
 		*head = tmp;

8b96debbfe92ee0d3b04fb85ad932ee15ca637c3
zel-khad <elkhadirzakaria48@gmail.com>
2024-06-08 08:51:37 +0100

    Refactor: Introduce t_token and modify gettoken() accordingly
    This commit introduces a new data structure, `t_token`, and modifies the `gettoken()` function to use it. This will allow us to store additional information about each token beyond just its type.

--- sources/minishell.c
+++ sources/minishell.c
@@ -1,8 +1,11 @@
 #include <minishell.h>
 
-void gettoken(char *line)
+t_token *gettoken(char *line)
 {
     t_token *token;
+
+    token = malloc(sizeof(t_token));
+
     int i ;
     
     char *whitespace;
@@ -19,41 +22,50 @@ void gettoken(char *line)
         if (ft_strchr(symbols, line[i]))
         {
             if (line[i] == '(')
-                add_node(token, LEFT_PARENTHESES);
+                add_node(&token, LEFT_PARENTHESES);
             else if (line[i] == ')')
-                add_node(token, RIGHT_PARENTHESES);
+                add_node(&token, RIGHT_PARENTHESES);
             else if (line[i] == '\\')
-                add_node(token, BACKSLASH);
+                add_node(&token, BACKSLASH);
             else if (line[i] == '"')
-                add_node(token, DOUBLE_QUOTATION);
+                add_node(&token, DOUBLE_QUOTATION);
             else if (line[i] == '\'')
-                add_node(token, SINGLE_QUOTATION);
+                add_node(&token, SINGLE_QUOTATION);
             else if (line[i] == '$')
-                add_node(token, DOLLAR_SIGN);
+                add_node(&token, DOLLAR_SIGN);
             else if (line[i] == '&')
-                add_node(token, AMPERSAND);
+                add_node(&token, AMPERSAND);
             else if (line[i] == '>')
-                add_node(token, SORTIE);
+                add_node(&token, SORTIE);
             else if (line[i] == '<')
-                add_node(token, ENTREE);
+                add_node(&token, ENTREE);
+            else if (line[i] == '|')
+                add_node(&token, PIPE);
         }
         i++;
     }
-    
-    printf("the index of token is %d\n", i);
-
+    return(token);
 }
 
-
+void print_(t_token *token)
+{
+    if (!token)
+        return;
+    printf("token is -> %c\n", token->type);
+    print_(token->next);
+}
 void minishell()
 {
     int id ;
     char *line;
+    t_token *token;
 
     while (1)
     {
         line = readline(COLOR_BOLD GRN "➜ minishell__v1 ✗ ");
-        gettoken(line);
+        token = gettoken(line);
+        print_(token);
+
 
 
         // char *argvrc[] = {cmd, NULL , NULL};

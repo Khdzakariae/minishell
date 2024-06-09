a37ce8a5228abecd1f897dff0b56adc7c1d9a224
Zakariae El-Khadir <zel-khad@c3r6p9.1337.ma>
2024-06-09 15:05:23 +0100

    Refactor: Optimize ft_strcpy and remove unnecessary functions
    - Improved `ft_strcpy` by using a for loop.
    - Removed unused `freeList` and `add_node` functions.

--- sources/utility_functions.c
+++ sources/utility_functions.c
@@ -1,17 +1,17 @@
 #include <minishell.h>
 
-char *ft_strcpy(char **dst, const char *src) {
-    if (src == NULL) {
+char *ft_strcpy(char **dst, const char *src) 
+{
+    size_t i = 0;
+
+    if (src == NULL) 
         return NULL;
-    }
 
     *dst = malloc(ft_strlen(src) + 1);
-    if (*dst == NULL) {
+    if (*dst == NULL)
         return NULL;
-    }
-
-    size_t i = 0;
-    while (src[i]) {
+    while (src[i]) 
+    {
         (*dst)[i] = src[i];
         i++;
     }
@@ -19,46 +19,43 @@ char *ft_strcpy(char **dst, const char *src) {
     return *dst;
 }
 
-void freeList(t_token *head) 
+// void freeList(t_token *head) 
+// {
+//     t_token *current = head;
+//     t_token *next;
+
+//     while (current != NULL) {
+//         next = current->next;
+//         free(current->type); 
+//         free(current);
+//         current = next;
+//     }
+// }
+
+
+void add_node(t_token **head, const char *value, int type) 
 {
-    t_token *current = head;
-    t_token *next;
-
-    while (current != NULL) {
-        next = current->next;
-        free(current->type); 
-        free(current);
-        current = next;
-    }
-}
-
-
-void add_node(t_token **head, const char *symbol) {
     t_token *tmp = malloc(sizeof(t_token));
-    if (tmp == NULL) {
-        // Handle malloc failure
+    if (tmp == NULL) 
+    {
         fprintf(stderr, "Memory allocation failed for new node.\n");
         return;
     }
-
-    if (ft_strcpy(&tmp->type, symbol) == NULL) {
-        // Handle strdup failure
-        fprintf(stderr, "Memory allocation failed for symbol copy.\n");
-        free(tmp);
-        return;
-    }
-
+    if (value == NULL)
+        tmp->value = NULL;
+    else
+        ft_strcpy(&tmp->value, value);
+    tmp->type = type;
     tmp->next = NULL;
 
-    if (*head == NULL) {
+    if (*head == NULL) 
         *head = tmp;
-    } else {
+    else 
+    {
         t_token *current = *head;
         while (current->next != NULL) {
             current = current->next;
         }
         current->next = tmp;
     }
-
-    puts("Node added successfully.");
 }

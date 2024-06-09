bcff67ca86e5c0070906f91765f9ba6e479926fb
Zakariae El-Khadir <zel-khad@c3r6p9.1337.ma>
2024-06-09 15:05:07 +0100

    Refactor: Convert token types in `minishell.c` to integers

--- sources/minishell.c
+++ sources/minishell.c
@@ -1,13 +1,12 @@
 #include <minishell.h>
- 
-
 
 t_token *gettoken(char *line)
 {
     t_token *token = NULL;
 
     int i ;
-    
+    int start = 0;
+    int end = 0 ;    
     char *whitespace;
     char *symbols;
 
@@ -19,38 +18,42 @@ t_token *gettoken(char *line)
         i++;
     while (line[i])
     {
+        if (line[i] != ' ')
+            start++;
+        else if ((line[i] >= 97) && (line[i] <= 122))
+            end++;
         if (ft_strchr(symbols, line[i]))
         {
             if (line[i] == '(')
-                add_node(&token, "(");
+                add_node(&token, NULL, LEFT_PARENTHESES);
             else if (line[i] == ')')
-                add_node(&token, ")");
+                add_node(&token, NULL, RIGHT_PARENTHESES);
             else if (line[i] == '\\')
-                add_node(&token, "\\");
+                add_node(&token, NULL, BACKSLASH);
             else if (line[i] == '"')
-                add_node(&token, "\"");
+                add_node(&token,NULL, DOUBLE_QUOTATION);
             else if (line[i] == '\'')
-                add_node(&token, "'");
+                add_node(&token, NULL, SINGLE_QUOTATION);
             else if (line[i] == '$')
-                add_node(&token, "$");
+                add_node(&token, NULL, DOLLAR_SIGN);
             else if (line[i] == '&' && line[i + 1] == '&')
             {
-                add_node(&token, "&&");
+                add_node(&token, NULL, AND);
                 i++;
             }
             else if (line[i] == '&')
-                add_node(&token, "&");
+                add_node(&token, NULL, AMPERSAND);
             else if (line[i] == '>')
-                add_node(&token, ">");
+                add_node(&token, NULL, SORTIE);
             else if (line[i] == '<')
-                add_node(&token, "<");
+                add_node(&token, NULL, ENTREE);
             else if (line[i] == '|' && line[i + 1] == '|')
             {
-                add_node(&token, "||");
+                add_node(&token, NULL, OR);
                 i++;
             }
             else if (line[i] == '|')
-                add_node(&token, "|");
+                add_node(&token, NULL, PIPE);
         }
         i++;
     }
@@ -61,7 +64,7 @@ void printList(t_token* head) {
     t_token* current = head;
 
     while (current != NULL) {
-        printf("%s\n", current->type);
+        printf("%d\n", current->type);
         current = current->next;
     }
     printf("\n");
@@ -70,8 +73,8 @@ void printList(t_token* head) {
 void minishell()
 {
     int id ;
-    char *line;
     t_token *token;
+    char *line;
 
     while (1)
     {
@@ -79,7 +82,7 @@ void minishell()
         chech_Quoting(line);
         token = gettoken(line);
         printList(token);
-        freeList(token);
+        // freeList(token);
         usleep(50);
 
 

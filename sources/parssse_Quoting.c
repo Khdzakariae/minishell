eb4e74cb0d661b4b310c0e9f65468a55e456e46f
Zakariae El-Khadir <zel-khad@c3r6p9.1337.ma>
2024-06-09 15:05:14 +0100

    Fix: Ignore 'check_SORTIE_and_ENTREE' in 'chech_Quoting'

--- sources/parssse_Quoting.c
+++ sources/parssse_Quoting.c
@@ -2,56 +2,65 @@
 
  bool check_PARENTHESES(char *line)
  {
-    int  count_left;
-    int  count_right;
+    int  flag;
     int i ;
 
     i = 0;
-    count_left = 0;
-    count_right = 0;
+    flag = 0;
     
     while (line[i])
     {
-        if (line[i] == ')')
-            count_right++;
-        else if (line[i] == '(')
-            count_left++;
+        if (line[i] == '(')
+            flag++;
+        else if (line[i] == ')')
+            flag--;
+        if (flag < 0)
+        {
+            printf(RED"ERROR PARANTES");
+            return(false);
+        }
         i++;
-    }
-    if (count_left != count_right)
-    {
-        printf(RED"ERROR PARANTES");
-        return(false);
     }
     return(true);
  }
 
 
- bool check_SORTIE_and_ENTREE(char *line)
- {
-    int  count_left;
-    int  count_right;
-    int i ;
+//  bool check_SORTIE_and_ENTREE(char *line)
+//  {
+//     int  count_left;
+//     int  count_right;
+//     int i ;
 
-    i = 0;
-    count_left = 0;
-    count_right = 0;
+//     i = 0;
+//     count_left = 0;
+//     count_right = 0;
     
-    while (line[i])
-    {
-        if (line[i] == '>')
-            count_right++;
-        else if (line[i] == '<')
-            count_left++;
-        i++;
-    }
-    if (count_left != count_right)
-    {
-        printf(RED"ERROR entre");
-        return(false);
-    }
-    return(true);
- }
+//     while (line[i])
+//     {
+//         if (line[i] == '>')
+//             count_right++;
+//         else if (line[i] == '<')
+//             count_left++;
+//         i++;
+//     }
+//     if (count_left != count_right)
+//     {
+//         printf(RED"ERROR entre");void freeList(t_token *head) 
+// {
+//     t_token *current = head;
+//     t_token *next;
+
+//     while (current != NULL) {
+//         next = current->next;
+//         free(current->type); 
+//         free(current);
+//         current = next;
+//     }
+// }
+//         return(false);
+//     }
+//     return(true);
+//  }
 
  bool check_DOUBLE_QUOTATION(char *line)
  {
@@ -100,7 +109,7 @@
  }
 bool chech_Quoting(char *line)
 {
-    if (check_PARENTHESES(line) == false || check_SORTIE_and_ENTREE(line) == false)
+    if (check_PARENTHESES(line) == false )
         return false;
     if (check_DOUBLE_QUOTATION(line) == false || check_SINGLE_QUOTATION(line))
         return false;

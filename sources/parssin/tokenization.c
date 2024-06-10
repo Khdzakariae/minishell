eecfc97966571a069bd88d61f589a94270f418d3
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2024-06-10 19:19:21 +0100

    Refactor: Remove redundant parsing functions for symbols and redirects

--- sources/parssin/tokenization.c
+++ sources/parssin/tokenization.c
@@ -6,79 +6,11 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:25:15 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/06/10 19:11:57 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/06/10 19:16:02 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
-
-void	parse_redirect_input(t_token **token, char *line, int *i)
-{
-	if (line[*i + 1] == '<')
-	{
-		add_node(token, NULL, LEFT_GUILLEMET);
-		(*i)++;
-	}
-	else
-		add_node(token, NULL, ENTREE);
-}
-
-void	parse_redirect_output(t_token **token, char *line, int *i)
-{
-	if (line[*i + 1] == '>')
-	{
-		add_node(token, NULL, RIGHT_GUILLEMET);
-		(*i)++;
-	}
-	else
-		add_node(token, NULL, SORTIE);
-}
-
-void	parse_ampersand(t_token **token, char *line, int *i)
-{
-	if (line[*i + 1] == '&')
-	{
-		add_node(token, NULL, AND);
-		(*i)++;
-	}
-	else
-		add_node(token, NULL, AMPERSAND);
-}
-
-void	parse_pipe(t_token **token, char *line, int *i)
-{
-	if (line[*i + 1] == '|')
-	{
-		add_node(token, NULL, OR);
-		(*i)++;
-	}
-	else
-		add_node(token, NULL, PIPE);
-}
-
-void	parse_symbol(t_token **token, char symbol, char *line, int *i)
-{
-	if (symbol == '(')
-		add_node(token, NULL, LEFT_PARENTHESES);
-	else if (symbol == ')')
-		add_node(token, NULL, RIGHT_PARENTHESES);
-	else if (symbol == '\\')
-		add_node(token, NULL, BACKSLASH);
-	else if (symbol == '"')
-		add_node(token, NULL, DOUBLE_QUOTATION);
-	else if (symbol == '\'')
-		add_node(token, NULL, SINGLE_QUOTATION);
-	else if (symbol == '$')
-		add_node(token, NULL, DOLLAR_SIGN);
-	else if (symbol == '&')
-		parse_ampersand(token, line, i);
-	else if (symbol == '<')
-		parse_redirect_input(token, line, i);
-	else if (symbol == '>')
-		parse_redirect_output(token, line, i);
-	else if (symbol == '|')
-		parse_pipe(token, line, i);
-}
 
 int	extract_word(t_token **token, char *line, int i)
 {

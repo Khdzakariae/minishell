c32e09ea6ee2ce9b58f672c3d8348c0367ce1fe2
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2024-06-10 10:40:38 +0100

    Fix: Parse ampersands and pipes correctly
    Modifies the tokenization function to correctly parse ampersands ('&') and pipes ('|').

--- sources/parssin/tokenization.c
+++ sources/parssin/tokenization.c
@@ -1,51 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenization.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 10:25:15 by zel-khad          #+#    #+#             */
+/*   Updated: 2024/06/10 10:31:10 by zel-khad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <minishell.h>
 
-void parse_ampersand(t_token **token, char *line, int *i) 
+void	parse_ampersand(t_token **token, char *line, int *i)
 {
-    if (line[*i + 1] == '&') 
+	if (line[*i + 1] == '&')
 	{
-        add_node(token, NULL, AND);
-        (*i)++;
-    }
-    else 
-        add_node(token, NULL, AMPERSAND);
-}
-
-void parse_pipe(t_token **token, char *line, int *i) 
-{
-    if (line[*i + 1] == '|')
-	{
-        add_node(token, NULL, OR);
-        (*i)++;
+		add_node(token, NULL, AND);
+		(*i)++;
 	}
-    else 
-        add_node(token, NULL, PIPE);
+	else
+		add_node(token, NULL, AMPERSAND);
 }
 
-void parse_symbol(t_token **token, char symbol, char *line, int *i) 
+void	parse_pipe(t_token **token, char *line, int *i)
+{
+	if (line[*i + 1] == '|')
+	{
+		add_node(token, NULL, OR);
+		(*i)++;
+	}
+	else
+		add_node(token, NULL, PIPE);
+}
+
+void	parse_symbol(t_token **token, char symbol, char *line, int *i)
 {
 	if (symbol == '(')
 		add_node(token, NULL, LEFT_PARENTHESES);
 	else if (symbol == ')')
 		add_node(token, NULL, RIGHT_PARENTHESES);
-    else if (symbol == '\\') 
-        add_node(token, NULL, BACKSLASH);
-    else if (symbol == '"') 
-        add_node(token, NULL, DOUBLE_QUOTATION);
-    else if (symbol == '\'')
-        add_node(token, NULL, SINGLE_QUOTATION);
-    else if (symbol == '$') 
-        add_node(token, NULL, DOLLAR_SIGN);
-    else if (symbol == '&') 
-        parse_ampersand(token, line, i);
-    else if (symbol == '>') 
-        add_node(token, NULL, SORTIE);
-    else if (symbol == '<') 
-        add_node(token, NULL, ENTREE);
-    else if (symbol == '|') 
-        parse_pipe(token, line, i);
+	else if (symbol == '\\')
+		add_node(token, NULL, BACKSLASH);
+	else if (symbol == '"')
+		add_node(token, NULL, DOUBLE_QUOTATION);
+	else if (symbol == '\'')
+		add_node(token, NULL, SINGLE_QUOTATION);
+	else if (symbol == '$')
+		add_node(token, NULL, DOLLAR_SIGN);
+	else if (symbol == '&')
+		parse_ampersand(token, line, i);
+	else if (symbol == '>')
+		add_node(token, NULL, SORTIE);
+	else if (symbol == '<')
+		add_node(token, NULL, ENTREE);
+	else if (symbol == '|')
+		parse_pipe(token, line, i);
 }
-
 
 int	extract_word(t_token **token, char *line, int i)
 {
@@ -68,9 +79,9 @@ int	extract_word(t_token **token, char *line, int i)
 t_token	*gettoken(char *line)
 {
 	t_token	*token;
-	int		i;
 	char	*whitespace;
 	char	*symbols;
+	int		i;
 
 	token = NULL;
 	i = 0;

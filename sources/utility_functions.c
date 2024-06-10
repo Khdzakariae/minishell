9866ffa5528ab9363d8064a1c807fd1cfb1c8087
Zakariae El-Khadir <zel-khad@c3r6p9.1337.ma>
2024-06-10 09:53:13 +0100

    Fix: Update ft_strcpy implementation in utility_functions.c

--- sources/utility_functions.c
+++ sources/utility_functions.c
@@ -1,64 +1,66 @@
 #include <minishell.h>
 #include <stdio.h>
-char *ft_strcpy(char **dst, const char *src) 
+
+char	*ft_strcpy(char **dst, const char *src)
 {
-    size_t i = 0;
+	size_t	i;
 
-    if (src == NULL) 
-        return NULL;
-
-    *dst = malloc(ft_strlen(src) + 1);
-    if (*dst == NULL)
-        return NULL;
-    while (src[i]) 
-    {
-        (*dst)[i] = src[i];
-        i++;
-    }
-    (*dst)[i] = '\0';
-
-    return *dst;
+	i = 0;
+	if (src == NULL)
+		return (NULL);
+	*dst = malloc(ft_strlen(src) + 1);
+	if (*dst == NULL)
+		return (NULL);
+	while (src[i])
+	{
+		(*dst)[i] = src[i];
+		i++;
+	}
+	(*dst)[i] = '\0';
+	return (*dst);
 }
 
-void freeList(t_token *head) 
+void	freeList(t_token *head)
 {
-    t_token *current = head;
-    t_token *next;
+	t_token	*current;
+	t_token	*next;
 
-    while (current != NULL) {
-        next = current->next;
-        free(current->value); 
-        free(current);
-        current = next;
-    }
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->value);
+		free(current);
+		current = next;
+	}
 }
 
-
-void add_node(t_token **head, const char *value, int type) 
+void	add_node(t_token **head, const char *value, int type)
 {
-    t_token *tmp = malloc(sizeof(t_token));
-    if (tmp == NULL) 
-    {
-        fprintf(stderr, "Memory allocation failed for new node.\n");
-        return;
-    }
-    if (value == NULL)
-        tmp->value = NULL;
-    else
-        ft_strcpy(&tmp->value, value);
-    tmp->type = type;
-    tmp->next = NULL;
+	t_token *tmp = malloc(sizeof(t_token));
+	if (tmp == NULL)
+	{
+		fprintf(stderr, "Memory allocation failed for new node.\n");
+		return ;
+	}
+	if (value == NULL)
+		tmp->value = NULL;
+	else
+		ft_strcpy(&tmp->value, value);
+	tmp->type = type;
+	tmp->next = NULL;
 
-    if (*head == NULL) 
-        *head = tmp;
-    else 
-    {
-        t_token *current = *head;
-        while (current->next != NULL) {
-            current = current->next;
-        }
-        current->next = tmp;
-    }
-        printf("-----> type %d\n", tmp->type);
-        printf("-----> value %s\n", tmp->value);
+	if (*head == NULL)
+		*head = tmp;
+	else
+	{
+		t_token *current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = tmp;
+	}
+	printf("-----> type %d\n", tmp->type);
+	printf("-----> value %s\n", tmp->value);
 }

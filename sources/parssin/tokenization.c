cf8d92df04716f975ef746ac6bbfd828b2b6d144
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2024-06-10 19:14:58 +0100

    feat: Add redirection parsing
    Adds support for parsing input and output redirection in the tokenizer.

--- sources/parssin/tokenization.c
+++ sources/parssin/tokenization.c
@@ -6,11 +6,33 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:25:15 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/06/10 10:31:10 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/06/10 19:11:57 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+void	parse_redirect_input(t_token **token, char *line, int *i)
+{
+	if (line[*i + 1] == '<')
+	{
+		add_node(token, NULL, LEFT_GUILLEMET);
+		(*i)++;
+	}
+	else
+		add_node(token, NULL, ENTREE);
+}
+
+void	parse_redirect_output(t_token **token, char *line, int *i)
+{
+	if (line[*i + 1] == '>')
+	{
+		add_node(token, NULL, RIGHT_GUILLEMET);
+		(*i)++;
+	}
+	else
+		add_node(token, NULL, SORTIE);
+}
 
 void	parse_ampersand(t_token **token, char *line, int *i)
 {
@@ -50,10 +72,10 @@ void	parse_symbol(t_token **token, char symbol, char *line, int *i)
 		add_node(token, NULL, DOLLAR_SIGN);
 	else if (symbol == '&')
 		parse_ampersand(token, line, i);
-	else if (symbol == '>')
-		add_node(token, NULL, SORTIE);
 	else if (symbol == '<')
-		add_node(token, NULL, ENTREE);
+		parse_redirect_input(token, line, i);
+	else if (symbol == '>')
+		parse_redirect_output(token, line, i);
 	else if (symbol == '|')
 		parse_pipe(token, line, i);
 }

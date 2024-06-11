7adc3ff724216a930bb80bd30290a440aee7b0aa
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2024-06-11 10:40:09 +0100

    Refactor: Improve tokenization to handle string literals and generate commands
    - Added support for string literals in tokenization.
    - Refactored code to generate commands from tokens.

--- sources/parssin/tokenization.c
+++ sources/parssin/tokenization.c
@@ -6,11 +6,26 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:25:15 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/06/10 19:16:02 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/06/11 10:39:45 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+t_token *generet_cmd(t_token *token)
+{
+	t_token *tmp = token;
+	
+	tmp->type = CMD;
+	while (tmp->next)
+	{
+		if (tmp->type != 1 && tmp->type != 0 )
+			tmp->next->type = CMD;
+		tmp = tmp->next;
+	}
+	tmp = token;
+	return (token);
+}
 
 int	extract_word(t_token **token, char *line, int i)
 {
@@ -30,6 +45,24 @@ int	extract_word(t_token **token, char *line, int i)
 	return (i);
 }
 
+void lexer_collect_string(t_token **token, char *line, int *i)
+{
+	int start;
+	char *tmp;
+	start = *i;
+	
+	while (line[*i] != '"')
+		(*i)++;
+	if (line[*i] == '"')
+	{
+		tmp = ft_substr(line, start, (size_t)(i - start));
+		add_node(token, tmp, WORD);
+		*i = *i + ft_strlen(tmp);
+	}
+	else
+		printf("ERROR");
+}
+
 t_token	*gettoken(char *line)
 {
 	t_token	*token;
@@ -40,16 +73,19 @@ t_token	*gettoken(char *line)
 	token = NULL;
 	i = 0;
 	whitespace = " \t\r\n\v";
-	symbols = " <|>&()'\"";
+	symbols = " <|>&()'";
 	while (line[i] && ft_strchr(whitespace, line[i]))
 		i++;
 	while (line[i])
 	{
-		if (!ft_strchr(symbols, line[i]) && !ft_strchr(whitespace, line[i]))
+		if (line[i] == '"')
+			lexer_collect_string(&token, line, &i);
+		else if  (!ft_strchr(symbols, line[i]) && !ft_strchr(whitespace, line[i]))
 			i = extract_word(&token, line, i) - 1;
 		else
 			parse_symbol(&token, line[i], line, &i);
 		i++;
 	}
+	token = generet_cmd(token);
 	return (token);
 }

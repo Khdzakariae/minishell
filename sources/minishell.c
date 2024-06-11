2ada079a5d88f97d8d85a0be4da0f8618a9a8839
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2024-06-11 16:37:12 +0100

    Fix: Added missing files and refactors with utility functions

---
+++ sources/minishell.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 10:33:51 by zel-khad          #+#    #+#             */
+/*   Updated: 2024/06/11 16:26:03 by zel-khad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+void	printlist(t_token *head)
+{
+	t_token	*current;
+
+	current = head;
+	while (current != NULL)
+	{
+		printf("-----> type %d\n", current->type);
+		printf("-----> value %s\n", current->value);
+		current = current->next;
+	}
+	printf("\n");
+}
+
+void	minishell(void)
+{
+	t_token	*token;
+	char	*line;
+
+	while (1)
+	{
+		line = readline(COLOR_BOLD "➜ minishell__v1 ✗ ");
+		if (line == NULL)
+		{
+			printf("exit\n");
+			exit(0);
+		}
+		chech_quoting(line);
+		token = gettoken(line);
+		printlist(token);
+		freelist(token);
+		usleep(50);
+	}
+}

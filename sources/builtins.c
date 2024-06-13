eaf83f69679f54af907338a5726af394be3e971f
ZAKARIAE EL-KHADIR <zel-khad@ZAKARIAEs-MacBook-Pro.local>
2024-06-13 19:46:22 +0100

    feat: Add cd builtin
    Adds `cd` builtin to builtins.c, allowing users to change the current working directory.

--- sources/builtins.c
+++ sources/builtins.c
@@ -6,10 +6,25 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 11:30:33 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/06/11 11:30:42 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/06/11 22:16:23 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+
+void builtins(t_token *token)
+{
+    if (ft_strcmp(token->value, "cd") == 0)
+    {
+     if(chdir(token->next->value) == 0) {
+        printf("Directory changed successfully to %s\n", token->value);
+    } else {
+        perror("chdir() error");
+    }
+    }
+    else
+        puts("zeeb");
+}
 
 

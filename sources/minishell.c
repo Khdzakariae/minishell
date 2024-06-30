59b241860b4413ceb0d04d77c273dadac2f795ca
Zakariae El-Khadir <zel-khad@c3r8p6.1337.ma>
2024-06-30 10:18:38 +0100

    Enhance: Refactor generet_cmd() with error checking and command parsing logic

--- sources/minishell.c
+++ sources/minishell.c
@@ -26,29 +26,30 @@ void	printlist(t_token *head)
 	printf("\n");
 }
 
-void	printlist_(t_cmd *head)
+void printlist_(t_cmd *head)
 {
-	t_cmd	*current;
+    t_cmd *current = head;
 
-	int i = 0;
-	current = head;
-	while (current != NULL)
-	{
-		printf("-----> type %d\n", current->type);
-		while (current->value[i])
-		{
-			printf("-----> value %s\n", current->value[i]);
-			i++;
-			/* code */
-		}
-		
-		current = current->next;
-	}
-	printf("\n");
+    while (current != NULL)
+    {
+
+        printf("-----> type %d\n", current->type);
+        // Print values if present
+
+            int i = 0;
+            while ((current->value != NULL) && (current->value[i]))
+            {
+                printf("-----> value %s\n", current->value[i]);
+                i++;
+            }
+        current = current->next;
+    }
+    printf("\n");
 }
+
+
 int count_word(t_token **tmp)
 {
-    
     int count_word = 0;
     while ((*tmp))
     {
@@ -58,52 +59,94 @@ int count_word(t_token **tmp)
             break;
         *tmp = (*tmp)->next;
     }
+	puts("=======================================================");
+    printf("the cant value is -----> |%d|\n", count_word);
+	puts("=======================================================");
     return count_word;
 }
 
-t_cmd *generate_tab_cmd(t_cmd *cmd_, t_token *token, int count)
+char  **generate_tab_cmd(char **cmd_, t_token *token, int count)
 {
     int i = 0;
-    cmd_->type = WORD;
     while (i < count)
     {
+
         if (token->type == WORD)
         {
-            cmd_->value[i] = ft_strcpy(&cmd_->value[i], token->value);
+            cmd_[i] = ft_strcpy(&cmd_[i], token->value);
             i++;
         }
+		// (*cmd_) = (*cmd_)->next;
         token = token->next;
     }
-    cmd_->value[i] = NULL;
-    return cmd_;
+    cmd_[i] = NULL;
+    return(cmd_);
 }
 
+// void  generate_tab_cmd(t_cmd **cmd_, t_token *token, int count)
+// {
+//     int i = 0;
+//     (*cmd_)->type = WORD;
+//     while (i < count)
+//     {
 
+//         if (token->type == WORD)
+//         {
+//             (*cmd_)->value[i] = ft_strcpy(&(*cmd_)->value[i], token->value);
+//             i++;
+//         }
+// 		// (*cmd_) = (*cmd_)->next;
+//         token = token->next;
+//     }
+//     (*cmd_)->value[i] = NULL;
+// }
 
-t_cmd *generet_cmd(t_token *token, t_cmd *cmd_)
+void print_tab(char **tab)
+{
+    int i = 0;
+    while (tab[i])
+    {
+        printf("tabde[%d] is |%s|\n", i, tab[i]);
+        i++;
+    }
+}
+
+void generet_cmd(t_token *token, t_cmd **cmd_)
 {
     t_token *tmp;
     t_token *tmp1;
+    char **tab;
     int count = 0;
     tmp = token;
     tmp1 = token;
     while (tmp)
     {
-        if (tmp1->type == WORD)
+        // puts("============================== debag ==================================");
+        // printf("the token type is |%d|\n", token->type);
+        // printf("the tmp type is |%d|\n", token->type);
+        // puts("========================================================================");
+        if (tmp->type == WORD)
         {
             count = count_word(&tmp);
-            add_node_(&cmd_);
-            cmd_->value = malloc((count + 1) * sizeof(char *));
-            cmd_ = generate_tab_cmd(cmd_, tmp1, count);
-			
+            tab = malloc((count + 1) * sizeof(char *));
+            tab = generate_tab_cmd(tab, token, count);
+            // generate_tab_cmd(&cmd_, tmp1, count);
+            // add_node_(cmd_ , 0);
         }
-			// exit(20);
-        if (tmp == NULL)
+		// else if (tmp != NULL && tmp->type != SPACE_)
+		// {
+        //     puts("=====================");
+        //     add_node_(&cmd_, tmp->type);
+        //     if (tmp->next != NULL)
+        //         tmp = tmp->next;
+		// }
+        if (tmp == NULL || tmp == NULL)
             break;
-        tmp = tmp->next;
+		tmp = tmp->next;
     }
-    return cmd_;
+    print_tab(tab);
 }
+
 
 void	minishell(void)
 {
@@ -125,12 +168,13 @@ void	minishell(void)
 		token = gettoken(line);
 		printlist(token);
 		puts("===========================");
-		cmd_ = generet_cmd(token, cmd_);
+		generet_cmd(token, &cmd_);
 		printlist_(cmd_);
 
 		// builtins(token);
-		freelist(token);
+	    // freelist_(cmd_);
 		usleep(50);
 	}
+		freelist(token);
 	clear_history();
 }

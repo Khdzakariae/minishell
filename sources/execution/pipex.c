915043edca5e5512eae61a626fc9ba032fa1e660
Ayoub Ogbi <aogbi@c3r8p5.1337.ma>
2024-07-01 00:44:45 +0100

    Refactor: Use pipex.c instead of pipex.h
    - Refactor code to use pipex.c directly instead of including pipex.h
    - Remove unnecessary function return type in execute_command()
    - Implement wait() in main() to wait for all child processes to finish
    - Use pipex.c for piping commands and freeing allocated memory

--- sources/execution/pipex.c
+++ sources/execution/pipex.c
@@ -6,7 +6,7 @@
 /*   By: aogbi <aogbi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 16:53:37 by aogbi             #+#    #+#             */
-/*   Updated: 2024/06/29 19:41:44 by aogbi            ###   ########.fr       */
+/*   Updated: 2024/07/01 00:41:54 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,55 +44,83 @@ char	*cmd_path(char *cmd, char **path)
     return (NULL);
 }
 
-int	execute_command(char **cmd, char **path, int in_fd, int out_fd, char **env)
+void	execute_command(char **cmd, char **path, int in_fd, int out_fd, char **env)
 {
 	pid_t pid;
 	char *cmd_name;
 
-	pid = fork();
-	if (pid == 0)
-	{
-		if (in_fd != STDIN_FILENO)
-			dup2(in_fd, STDIN_FILENO);
-		if (out_fd != STDOUT_FILENO)
-			dup2(out_fd, STDOUT_FILENO);
+    pid = fork();
+	if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        if (in_fd != STDIN_FILENO)
+        {
+            dup2(in_fd, STDIN_FILENO);
+            close(in_fd);
+        }
+        if (out_fd != STDOUT_FILENO)
+        {
+            dup2(out_fd, STDOUT_FILENO);
+            close(out_fd);
+        }
 		cmd_name = cmd_path(cmd[0], path);
 		if (cmd_name)
 			execve(cmd_name, cmd, env);
 		exit(EXIT_FAILURE);
 	}
-	else
+}
+
+void del(void *content)
+{
+	char **str;
+	int i = 0;
+
+	str = (char **)content;
+	while(str[i])
 	{
-		if (in_fd != STDIN_FILENO)
-			close(in_fd);
-		if (out_fd != STDOUT_FILENO)
-			close(out_fd);
+		free(str[i]);
+        i++;
 	}
-	return pid;
+	free(str);
 }
 
 void	pipex(t_list *list, int size, char **env, int in_fd, int out_fd)
 {
 	char **path;
 	int fd[2];
-	pid_t pid;
 
 	path = ft_split(find_path_from_env(env), ':');
 	while (list->next)
 	{
-		pipe(fd);
-		pid = execute_command(list->content, path, in_fd, fd[1], env);
+		if (pipe(fd) == -1)
+		{
+			perror("pipe");
+			exit(EXIT_FAILURE);
+		}
+		execute_command(list->content, path, in_fd, fd[1], env);
+        close(fd[1]);
 		in_fd = fd[0];
 		list = list->next;
 	}
-	pipe(fd);
+	if (pipe(fd) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
 	close(fd[1]);
-	dup2(STDOUT_FILENO, STDOUT_FILENO);
-	execute_command(list->content, path, fd[0], out_fd, env);
+	execute_command(list->content, path, in_fd, out_fd, env);
+	del(path);
+	while(wait(NULL) > 0);
 }
+
 int main(int argc, char *argv[], char *env[])
 {
-	t_list    *list;
+	t_list	*list;
+	t_list	*save;
 	int i = 1;
 
 	list = NULL;
@@ -100,10 +128,12 @@ int main(int argc, char *argv[], char *env[])
 		return 0;
 	while(argc - i)
 	{
-		ft_lstadd_front(&list, ft_lstnew((void *)((char **)ft_split(argv[i], ' '))));
+		ft_lstadd_back(&list, ft_lstnew((void *)((char **)ft_split(argv[i], ' '))));
 		i++;
 	}
-    pipex(list, argc - 1, env, STDIN_FILENO, STDOUT_FILENO);
-	wait(&i);
+	int fd = open("test.txt", O_WRONLY); 
+	save = list;
+    pipex(list, argc - 1, env, STDIN_FILENO, fd);
+    ft_lstclear(&save, del);
     return 0;
 }

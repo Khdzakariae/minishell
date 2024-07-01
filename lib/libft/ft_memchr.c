a84a1c1de89bda67c1e3d595ba331df5455ad28a
Ayoub Ogbi <aogbi@c3r8p4.1337.ma>
2024-07-01 22:56:21 +0100

    Refactor: Rename variable 'ptr' to 'p' in ft_memchr

--- lib/libft/ft_memchr.c
+++ lib/libft/ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aogbi <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/03 15:46:54 by zel-khad          #+#    #+#             */
-/*   Updated: 2023/11/12 11:45:04 by zel-khad         ###   ########.fr       */
+/*   Created: 2023/11/02 21:03:05 by aogbi             #+#    #+#             */
+/*   Updated: 2023/11/18 13:22:50 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
-	unsigned char	*ptr;
+	unsigned char	*p;
 
+	p = (unsigned char *)s;
 	i = 0;
-	ptr = (unsigned char *)s;
-	while (i < n)
+	while (n - i)
 	{
-		if (ptr[i] == (unsigned char)c)
-		{
-			return ((void *)(ptr + i));
-		}
+		if (*(p + i) == (unsigned char)c)
+			return ((void *)(s + i));
 		i++;
 	}
 	return (NULL);

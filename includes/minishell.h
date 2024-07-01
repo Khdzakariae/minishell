b61961e6b31814a48d8df2c5e565288328c67861
Ayoub Ogbi <aogbi@c3r8p4.1337.ma>
2024-07-01 22:46:56 +0100

    Refactor: Move parssin.h and builtins.h includes
    This change moves the includes for parssin.h and builtins.h to the correct order to avoid compiler errors.

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,21 +6,20 @@
 /*   By: aogbi <aogbi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:39:00 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/06/28 22:26:33 by aogbi            ###   ########.fr       */
+/*   Updated: 2024/07/01 22:42:23 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <parssin.h>
 # include <execution.h>
-# include "../lib/libft/libft.h"
+# include <parssin.h>
 
 char	*ft_strcpy_1(char **dst, const char *src);
 
-int	ft_strcmp(char *s1, char *s2);
-void builtins(t_token *token);
+int		ft_strcmp(char *s1, char *s2);
+void	builtins(t_token *token);
 void	minishell(void);
 
 #endif

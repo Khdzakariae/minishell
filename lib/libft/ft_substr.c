4ff543376146bc317cb8990ad3b8d38d30c3dfb3
Zakariae El-Khadir <zel-khad@c3r8p8.1337.ma>
2024-07-01 21:15:33 +0100

    Refactor: Improve ft_substr to handle invalid start positions

--- lib/libft/ft_substr.c
+++ lib/libft/ft_substr.c
@@ -14,17 +14,41 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*ptr;
+	char	*str;
+	size_t	i;
 
 	if (!s)
 		return (NULL);
-	if (start > ft_strlen(s))
-		return (ft_strdup(""));
+	if (ft_strlen(s) <= start)
+	{
+		str = malloc(1);
+		if (!str)
+			return (NULL);
+		str[0] = '\0';
+		return (str);
+	}
 	if (len > ft_strlen(s) - start)
 		len = ft_strlen(s) - start;
-	ptr = (char *)ft_calloc(len + 1, sizeof(char));
-	if (!ptr)
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
 		return (NULL);
-	ft_strlcpy(ptr, s + start, len + 1);
-	return (ptr);
+	i = 0;
+	ft_strlcpy(str, s + start, len + 1);
+	return (str);
 }
+// char	*ft_substr(char const *s, unsigned int start, size_t len)
+// {
+// 	char	*ptr;
+
+// 	if (!s)
+// 		return (NULL);
+// 	if (start > ft_strlen(s))
+// 		return (ft_strdup(""));
+// 	if (len > ft_strlen(s) - start)
+// 		len = ft_strlen(s) - start;
+// 	ptr = (char *)ft_calloc(len + 1, sizeof(char));
+// 	if (!ptr)
+// 		return (NULL);
+// 	ft_strlcpy(ptr, s + start, len + 1);
+// 	return (ptr);
+// }

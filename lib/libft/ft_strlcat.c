9d494dfa55f6290cbe0fecbff7bce4da39ff140c
Ayoub Ogbi <aogbi@c3r8p4.1337.ma>
2024-07-01 22:57:20 +0100

    Fix: Update ft_strlcat function signature and implementation
    - Updates function signature to match prototype
    - Adds size checks to accommodate size-limited destinations
    - Calculates total length and limits copying to size - 1
    - Adds null terminator to destination

--- lib/libft/ft_strlcat.c
+++ lib/libft/ft_strlcat.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aogbi <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/31 20:11:04 by zel-khad          #+#    #+#             */
-/*   Updated: 2023/11/01 11:23:08 by zel-khad         ###   ########.fr       */
+/*   Created: 2023/11/03 02:29:55 by aogbi             #+#    #+#             */
+/*   Updated: 2023/11/18 13:28:27 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,27 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
+	size_t	len_dest;
+	size_t	len_src;
 	size_t	i;
-	size_t	len;
-	size_t	j;
+	size_t	ret;
 
-	if (dst == NULL && size == 0)
-		return (ft_strlen(src));
-	len = ft_strlen(src) + ft_strlen(dst);
-	i = ft_strlen(dst);
-	j = 0;
-	if (size == 0 || size < i)
-		return (ft_strlen(src) + size);
-	while (i < size - 1 && src[j])
+	i = 0;
+	len_dest = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	if (size == 0)
+		return (len_src);
+	if (len_dest > size)
+		return (len_src + size);
+	else
 	{
-		dst[i] = src[j];
-		i++;
-		j++;
+		ret = len_dest + len_src;
+		while (src[i] && (len_dest + i) < (size - 1))
+		{
+			dst[len_dest + i] = src[i];
+			i++;
+		}
+		dst[len_dest + i] = '\0';
+		return (ret);
 	}
-	dst[i] = '\0';
-	return (len);
 }

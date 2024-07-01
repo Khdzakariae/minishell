2f77ff3e3b934e5d85f1022da1504068ffac46fb
Ayoub Ogbi <aogbi@c3r8p4.1337.ma>
2024-07-01 22:57:37 +0100

    Refactor: Improve optimization in ft_strncmp()

--- lib/libft/ft_strncmp.c
+++ lib/libft/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aogbi <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/02 09:46:50 by zel-khad          #+#    #+#             */
-/*   Updated: 2023/11/12 11:36:54 by zel-khad         ###   ########.fr       */
+/*   Created: 2023/11/02 14:43:27 by aogbi             #+#    #+#             */
+/*   Updated: 2023/11/18 13:30:42 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while ((i < n) && (s1[i] || s2[i]))
+	while ((*s1 || *s2) && n--)
 	{
-		if ((unsigned char)s1[i] != (unsigned char)s2[i])
-		{
-			break ;
-		}
-		i++;
+		if (*s1 != *s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		++s1;
+		++s2;
 	}
-	if (i == n || (s1[i] == '\0' && s2[i] == '\0'))
-	{
-		return (0);
-	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
 }

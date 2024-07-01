69639f1fdd4da0c242cd841741b8d94aa57ba0da
Ayoub Ogbi <aogbi@c3r8p4.1337.ma>
2024-07-01 22:47:06 +0100

    feat: Generate and print cmd structure
    This commit introduces a new function, generet_cmd(), which parses the token list generated by the tokenizer and creates a cmd structure. The printlist_() function is also added to print the cmd structure, allowing for improved debugging and understanding of the cmd structure's contents.

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:33:51 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/06/13 22:03:35 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/07/01 14:15:58 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,34 @@ void	printlist(t_token *head)
 	printf("\n");
 }
 
+void printlist_(t_cmd *head)
+{
+    t_cmd *current = head;
+
+    while (current != NULL)
+    {
+
+        printf("-----> type %d\n", current->type);
+        // Print values if present
+
+            int i = 0;
+            while ((current->value != NULL) && (current->value[i]))
+            {
+                printf("-----> value %s\n", current->value[i]);
+                i++;
+            }
+        current = current->next;
+    }
+    printf("\n");
+}
+
 void	minishell(void)
 {
+	t_cmd	*cmd_;
 	t_token	*token;
 	char	*line;
 
+	cmd_ = NULL;
 	while (1)
 	{
 		line = readline(COLOR_BOLD "➜ minishell__v1 ✗ ");
@@ -42,10 +65,15 @@ void	minishell(void)
 		add_history(line);
 		chech_quoting(line);
 		token = gettoken(line);
-		// builtins(token);
+        puts("=================== stract token ======================");
 		printlist(token);
-		freelist(token);
-		usleep(50);
+        cmd_ = malloc(sizeof(t_cmd));
+        cmd_ = NULL;
+		generet_cmd(token, &cmd_);
+        puts("=================== stract cmd ======================");
+		printlist_(cmd_);
+        freelist_(cmd_);
+	    freelist(token);
 	}
 	clear_history();
 }

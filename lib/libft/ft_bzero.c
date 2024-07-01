39b4d564d7e5f067c154235b0e1c32d168787151
Ayoub Ogbi <aogbi@c3r8p4.1337.ma>
2024-07-01 22:55:49 +0100

    Fix: Update ft_bzero return value to match signature
    Co-authored-by: aogbi

--- lib/libft/ft_bzero.c
+++ lib/libft/ft_bzero.c
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aogbi <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/03 11:15:04 by zel-khad          #+#    #+#             */
-/*   Updated: 2023/11/12 11:44:31 by zel-khad         ###   ########.fr       */
+/*   Created: 2023/11/02 18:13:43 by aogbi             #+#    #+#             */
+/*   Updated: 2023/11/18 13:19:06 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+void	*ft_bzero(void *s, size_t n)
 {
 	size_t			i;
-	unsigned char	*ptr;
+	unsigned char	*p;
 
 	i = 0;
-	ptr = (unsigned char *)s;
-	while (i < n)
+	p = (unsigned char *)s;
+	while (n - i)
 	{
-		ptr[i] = 0;
-		i++;
+		p[i++] = 0;
 	}
+	return (s);
 }

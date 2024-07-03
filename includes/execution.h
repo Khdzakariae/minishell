45cb7f0a7641315ad874c615d83bda5befd79006
Ayoub Ogbi <aogbi@c3r6p6.1337.ma>
2024-07-03 06:07:45 +0100

    Refactor: Remove file merging from execution
    This commit refactors the execution module by removing the file merging functionality from the execution.h header file and into the parssin.c file. This separation of concerns improves code organization and reduces coupling between modules.

--- includes/execution.h
+++ includes/execution.h
@@ -6,21 +6,25 @@
 /*   By: aogbi <aogbi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 19:41:25 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/07/02 23:35:41 by aogbi            ###   ########.fr       */
+/*   Updated: 2024/07/03 03:45:48 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTION_H
 #define EXECUTION_H
 
+#include <minishell.h>
+#include <parssin.h>
 #include <fcntl.h>
 #include <errno.h>
 #include "../lib/libft/libft.h"
 
-void	pipex(t_list *list, char **env, t_list *in_list, t_list *out_list);
+void	pipex(t_list *list, char **env);
 
 int input_file(t_list *files);
 
 int output_file(t_list *files);
+
+t_list *merge(t_cmd *parssin);
 
 #endif

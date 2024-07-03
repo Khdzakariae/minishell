036c1fbbd476b1e05ea5dd40f8cd8232abc25241
Ayoub Ogbi <aogbi@c3r6p6.1337.ma>
2024-07-03 06:08:01 +0100

    Refactor: Merge function now returns a list of ogbi
    This commit refactors the merge function to return a list of ogbi structures, each containing a command, input files, and output files. This simplifies the subsequent pipeline processing.

--- sources/execution/merge.c
+++ sources/execution/merge.c
@@ -6,41 +6,59 @@
 /*   By: aogbi <aogbi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 23:48:56 by aogbi             #+#    #+#             */
-/*   Updated: 2024/07/03 00:39:08 by aogbi            ###   ########.fr       */
+/*   Updated: 2024/07/03 04:36:08 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <minishell.h>
+#include <execution.h>
 
-typedef struct s_ogbi
-{
-    char    **cmd;
-    t_list  *output_files;
-    t_list  *input_files;
-}   t_ogbi;
-
-void add_to_list(char **list, t_list **add)
+void add_to_list(void *contant, t_list **add)
 {
 	t_list *node;
+
+	node = ft_lstnew(contant);
+	ft_lstadd_back(add, node);
 }
 
-t_ogbi merge(t_cmd *parssin)
+void set_to_null(t_list **input_files, t_list **output_files)
 {
-	char	**cmd;
+   	*input_files = NULL;
+    *output_files = NULL;
+}
+
+t_list *merge(t_cmd *parssin)
+{
+	t_list	*node;
+	t_ogbi	*ogbi;
+	char    **command;
     t_list	*input_files;
 	t_list	*output_files;
 
+	set_to_null(&input_files, &output_files);
+	node = NULL;
     while(parssin)
     {
         if (parssin->type == SORTIE)
-        {}
+			add_to_list(*(parssin->value), &output_files);
 		else if (parssin->type == ENTREE)
-		{}
+			add_to_list(*(parssin->value), &input_files);
 		else if (parssin->type == WORD)
-		{}
+			command = parssin->value;
 		else if (parssin->type == PIPE)
-		{}
+		{
+			ogbi = malloc(sizeof(t_ogbi));
+			ogbi->cmd = command;
+			ogbi->input_files = input_files;
+			ogbi->output_files = output_files;
+			ft_lstadd_back(&node, ft_lstnew(ogbi));
+			set_to_null(&input_files, &output_files);
+        }
         parssin = parssin->next;
     }
-    return ();
+	ogbi = malloc(sizeof(t_ogbi));
+	ogbi->cmd = command;
+	ogbi->input_files = input_files;
+	ogbi->output_files = output_files;
+	ft_lstadd_back(&node, ft_lstnew(ogbi));
+    return (node);
 }

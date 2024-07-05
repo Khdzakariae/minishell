f2f1deb4035d1c109426b00324da4044ab395372
Zakariae El-Khadir <zel-khad@c3r8p7.1337.ma>
2024-07-05 18:41:05 +0100

    Refactor: Update tokenization logic for improved string handling

--- sources/parssin/tokenization.c
+++ sources/parssin/tokenization.c
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:25:15 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/07/01 21:07:45 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/07/05 18:33:37 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int	extract_word(t_token **token, char *line, int i)
 	return (i);
 }
 
+
 void lexer_collect_string(t_token **token, char *line, int *i)
 {
 	int start;
@@ -34,16 +35,15 @@ void lexer_collect_string(t_token **token, char *line, int *i)
 	start = *i;
 	int j = *i;
 	
-    while (line[*i] != '\0' && line[(*i) + 1] != '"')
+    while ((line[*i] != '\0') && (line[(*i) + 1] != '"' && line[(*i) + 1] != '\'' ))
 	{
         (*i)++;
 	}
-	if (line[j] == '"')
+	if (line[j] == '"' || line[j] == '\'')
 	{
 		(*i)++;
 		int length = *i - start;
-		tmp = ft_substr(line, start, length + 1);
-		printf("the word_____ is |%s|\n", tmp);
+		tmp = ft_substr(line, (start + 1), length - 1);
 		add_node(token, tmp, WORD);
 	}
 	else
@@ -63,12 +63,12 @@ t_token	*gettoken(char *line)
 	token = NULL;
 	i = 0;
 	whitespace = " \t\r\n\v";
-	symbols = " <|>&()'";
+	symbols = " <|>&()";
 	while (line[i] && ft_strchr(whitespace, line[i]))
 		i++;
 	while (line[i])
 	{
-		if (line[i] == '"')
+		if (line[i] == '"' || line[i] == '\'')
 			lexer_collect_string(&token, line, &i);
 		else if  (!ft_strchr(symbols, line[i]) && !ft_strchr(whitespace, line[i]))
 			i = extract_word(&token, line, i) - 1;

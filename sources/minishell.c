e65e4d5c653b81e5b4e8671219dd3ead97f97822
Zakariae El-Khadir <zel-khad@c3r8p7.1337.ma>
2024-07-05 12:41:56 +0100

    Refactor: Update author and messages colors

--- sources/minishell.c
+++ sources/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aogbi <aogbi@student.42.fr>                +#+  +:+       +#+        */
+/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:33:51 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/07/05 05:58:34 by aogbi            ###   ########.fr       */
+/*   Updated: 2024/07/05 12:33:05 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ void	minishell(void)
 	cmd_ = NULL;
 	while (1)
 	{
-		line = readline(COLOR_BOLD "➜ minishell__v1 ✗ ");
+		line = readline(COLOR_BOLD GRN"➜ minishell__v1 ✗ "RESET);
 		if (line == NULL)
 		{
 			printf("exit\n");
@@ -67,13 +67,13 @@ void	minishell(void)
 		add_history(line);
 		chech_quoting(line);
 		token = gettoken(line);
-        // puts("=================== stract token ======================");
-		// printlist(token);
+        puts("=================== stract token ======================");
+		printlist(token);
         cmd_ = malloc(sizeof(t_cmd));
         cmd_ = NULL;
 		generet_cmd(token, &cmd_);
-        // puts("=================== stract cmd ======================");
-		// printlist_(cmd_);
+        puts("=================== stract cmd ======================");
+		printlist_(cmd_);
 		list = merge(cmd_);
 		// char *type = ((t_red *)((t_list *)((t_ogbi *)list->content)->output_files)->content)->value;
 		// printf("%s\n", type);

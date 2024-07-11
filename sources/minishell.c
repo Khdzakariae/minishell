cded1c7c84b4cfe57a404d29e190eb41a5546832
Ayoub Ogbi <aogbi@c3r8p7.1337.ma>
2024-07-11 04:06:45 +0100

    Refactor: Update maintainer and remove debugging code

--- sources/minishell.c
+++ sources/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aogbi <aogbi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:33:51 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/07/05 20:27:40 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/07/11 03:43:17 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,8 +67,8 @@ void	minishell(void)
 		add_history(line);
 		// chech_quoting(line);
 		token = gettoken(line);
-        puts("=================== stract token ======================");
-		printlist(token);
+        // puts("=================== stract token ======================");
+		// printlist(token);
         cmd_ = malloc(sizeof(t_cmd));
         cmd_ = NULL;
 		generet_cmd(token, &cmd_);

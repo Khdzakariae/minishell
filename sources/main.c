d4854ba7690d346e6ffd2dd00b6775557a12adea
Zakariae El-Khadir <zel-khad@c3r6p10.1337.ma>
2024-07-23 17:50:34 +0100

    Refactor: Remove unused `expandEnvironmentVariable()`
    This commit removes the unused `expandEnvironmentVariable()` function from `main.c`, simplifying the codebase.

--- sources/main.c
+++ sources/main.c
@@ -6,21 +6,15 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:33:45 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/06/14 00:18:18 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/07/23 17:23:38 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-char *expandEnvironmentVariable(char *input)
-{
-    char *varValue ;
-    varValue = getenv(input);
-	return(varValue);
-}
+
 int main(void) 
 {
-	// printf("%s\n",expandEnvironmentVariable("HOME"));
 	signels_handler();
 	minishell();
     return 0;

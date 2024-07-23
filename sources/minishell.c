3d467ad36d3933597b98559921521b8bedf194ce
Zakariae El-Khadir <zel-khad@c3r6p10.1337.ma>
2024-07-23 17:50:41 +0100

    Fix: Check quoting syntax
    Fixes a syntax error that occurred when parsing input with incorrect quoting.

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:33:51 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/07/23 10:58:54 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/07/23 17:48:34 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,15 +65,19 @@ void	minishell(void)
 			exit(0);
 		}
 		add_history(line);
-		// chech_quoting(line);
 		token = gettoken(line);
+		if(chech_quoting(token) == false)
+		{
+			ft_putstr_fd("zash: syntax error near unexpected token\n", 2);
+			continue;
+		}
         // puts("=================== stract token ======================");
 		// printlist(token);
         cmd_ = malloc(sizeof(t_cmd));
         cmd_ = NULL;
 		generet_cmd(token, &cmd_);
-        puts("=================== stract cmd ======================");
-		printlist_(cmd_);
+        // puts("=================== stract cmd ======================");
+		// printlist_(cmd_);
 		list = merge(cmd_);
 		// char *type = ((t_red *)((t_list *)((t_ogbi *)list->content)->output_files)->content)->value;
 		// printf("%s\n", type);

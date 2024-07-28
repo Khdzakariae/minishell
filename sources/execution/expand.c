fc22af8469b302029b39c8fa7059a994600e3d1f
Zakariae El-Khadir <zel-khad@c3r8p6.1337.ma>
2024-07-28 14:09:20 +0100

    Refactor: Handle quoting and variable expansion
    This commit handles quoting within commands and expands environment variables.

--- sources/execution/expand.c
+++ sources/execution/expand.c
@@ -3,80 +3,157 @@
 /*                                                        :::      ::::::::   */
 /*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aogbi <aogbi@student.42.fr>                +#+  +:+       +#+        */
+/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 10:00:51 by aogbi             #+#    #+#             */
-/*   Updated: 2024/07/28 10:02:40 by aogbi            ###   ########.fr       */
+/*   Updated: 2024/07/28 14:09:02 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-void convert_variable_helper(char **cmd, int i, int j)
+void handle_quoting(char **cmd)
 {
-	char *value;
-	char *tmp;
-
-	if (!ft_strcmp(cmd[i] + j, "$?"))
-	{
-		cmd[i][j] = '\0';
-		tmp = ft_strjoin(cmd[i], ft_itoa(g_stat));
-		free(cmd[i]);
-		cmd[i] = tmp;
-	}
-	else if(!cmd[i][1])
-		return ;
-	else
-	{
-		value = getenv(cmd[i] + j + 1);
-		if (value)
-		{
-			cmd[i][j] = '\0';
-			tmp = ft_strjoin(cmd[i], ft_strdup(value));
-			free(cmd[i]);
-			cmd[i] = tmp;
-		}
-		else
-		{
-			if (j != 0)
-				cmd[i][j] = '\0';
-			else
-			{
-				j = i;
-				while(cmd[j])
-				{
-					free(cmd[j]);
-					cmd[j] = NULL;
-					if (cmd[j + 1])
-            	    	cmd[j] = ft_strdup(cmd[j + 1]);
-            	    j++;
-				}
-				i--;
-			}
-		}
-	}
-	return ;
-}
-
-void convert_variable(char **cmd)
-{
-	int i;
+	int i = 0;
 	int j;
 	
-	i = 0;
-	if (!cmd)
-		return;
+
+	while (cmd[i])
+	{
+		if (cmd[i][j] == '\"')
+		{
+			j++;
+			
+		}
+		else if (cmd[i][j] == '\'')
+		{
+			
+		}
+		else
+		
+	}
+	
+}
+
+void handle_quoting(char **cmd)
+{
+	int i = 0, j;
+	int singl_q = 0;
+	int double_q = 0;
+	int start = 0;
+	char *str = NULL;
+	char *tmp = NULL;
+	char *tmp1 = NULL;
+	
 	while(cmd[i])
 	{
 		j = 0;
-		while(cmd[i][j] != '$' && cmd[i][j])
+		while(cmd[i][j])
 		{
-			if (cmd[i][j] == '\'')
-				return ;
+			if (!singl_q && cmd[i][j] == '\"')
+			
+				double_q++;
+				if (double_q == 2)
+				
+					tmp = ft_substr(cmd[i], start, j - start);
+					if (tmp != NULL)
+					{
+						tmp1 = ft_strjoin(str, tmp);
+						free(str);
+						free(tmp);
+						str = tmp1;
+					}
+					else
+						str = tmp;
+					double_q == 0;
+				
+				start = j;
+			
+			else if (!double_q && cmd[i][j])
+			{
+				singl_q++;
+				if (singl_q == 2)
+					singl_q == 0;
+			}
 			j++;
 		}
-		if (cmd[i][j])
-			convert_variable_helper(cmd, i, j);
-        i++;
+		i++;	
 	}
+}
+
+
+
+
+
+// void convert_variable_helper(char **cmd, int i, int j)
+// {
+// 	char *value;
+// 	char *tmp;
+
+// 	if (!ft_strcmp(cmd[i] + j, "$?"))
+// 	{
+// 		cmd[i][j] = '\0';
+// 		tmp = ft_strjoin(cmd[i], ft_itoa(g_stat));
+// 		free(cmd[i]);
+// 		cmd[i] = tmp;
+// 	}
+// 	else if(!cmd[i][1])
+// 		return ;
+// 	else
+// 	{
+// 		value = getenv(cmd[i] + j + 1);
+// 		if (value)
+// 		{
+// 			cmd[i][j] = '\0';
+// 			tmp = ft_strjoin(cmd[i], ft_strdup(value));
+// 			free(cmd[i]);
+// 			cmd[i] = tmp;
+// 		}
+// 		else
+// 		{
+// 			if (j != 0)
+// 				cmd[i][j] = '\0';
+// 			else
+// 			{
+// 				j = i;
+// 				while(cmd[j])
+// 				{
+// 					free(cmd[j]);
+// 					cmd[j] = NULL;
+// 					if (cmd[j + 1])
+//             	    	cmd[j] = ft_strdup(cmd[j + 1]);
+//             	    j++;
+// 				}
+// 				i--;
+// 			}
+// 		}
+// 	}
+// 	return ;
+// }
+
+void convert_variable(char **cmd)
+{
+
+	 handle_quoting(cmd);
+
+	
+	// int i;
+	// int j;
+	
+	// i = 0;
+	// if (!cmd)
+	// 	return;
+	// while(cmd[i])
+	// {
+	// 	j = 0;
+	// 	while(cmd[i][j] != '$' && cmd[i][j])
+	// 	{
+	// 		if (cmd[i][j] == '\'')
+	// 			return ;
+	// 		j++;
+	// 	}
+	// 	if (cmd[i][j])
+	// 		convert_variable_helper(cmd, i, j);
+    //     i++;
+	// }
 }

e7d91bd74818a81d54ccb47cf9bbc4b3454a0e4c
Ayoub Ogbi <aogbi@c3r8p6.1337.ma>
2024-07-31 16:34:02 +0100

    Refactor: Update pipex to support environment variables
    - Added functions to work with environment variables.
    - Updated pipex to use these functions.

--- sources/execution/pipex.c
+++ sources/execution/pipex.c
@@ -6,7 +6,7 @@
 /*   By: aogbi <aogbi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 16:53:37 by aogbi             #+#    #+#             */
-/*   Updated: 2024/07/31 09:15:14 by aogbi            ###   ########.fr       */
+/*   Updated: 2024/07/31 16:33:25 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,11 +129,40 @@ void    ft_echo(char **cmd)
     	write(1, "\n", 1);
 }
 
+int	find_index_from_env(char **env, char *str, int type)
+{
+	int i;
+	int len;
+	char *tmp;
+
+	if (type == 1)
+		tmp = ft_strjoin(str, "=");
+	else
+		tmp = ft_strdup(str);
+	i = 0;
+	len = ft_strlen(tmp);
+	if (!env)
+		return (-1);
+    while (env[i])
+    {
+        if (ft_strncmp(env[i], tmp, len) == 0)
+		{
+			free(tmp);
+            return (i);
+		}
+        i++;
+    }
+	free(tmp);
+    return (-1);
+}
+
 void	ft_env(char **env)
 {
 	int i;
 
     i = 0;
+	if (!env)
+		return ;
     while (env[i])
     {
         printf("%s\n", env[i]);
@@ -152,25 +181,175 @@ int count_array_of_str(char **env)
 	return (i);
 }
 
+void ft_unset(char **cmd, t_export *env_list)
+{
+	int i;
+	int index;
+	char **tmp;
 
-char **ft_export(char **cmd, char **env)
+	i = 1;
+	while(cmd[i])
+	{
+		index = find_index_from_env(env_list->export, cmd[i], 2);
+		if(index >= 0)
+		{
+			tmp = array_remove(env_list->export, index);
+			free(env_list->export);
+			env_list->export = tmp;
+		}
+		else
+		{
+			index = find_index_from_env(env_list->env, cmd[i], 1);
+			if(index >= 0)
+			{
+				tmp = array_remove(env_list->env, index);
+				free(env_list->env);
+				env_list->env = tmp;
+			}
+		}
+		i++;
+	}
+}
+
+
+void add_in_env(char *cmd, t_export *env_list, int j)
+{
+	int index;
+	int len;
+	char **tmp;
+
+	cmd[j] = '\0';
+	index = find_index_from_env(env_list->export, cmd,2 );
+	if (index >= 0)
+	{
+		tmp = array_remove(env_list->export, index);
+		free(env_list->export);
+		env_list->export = tmp;
+	}
+	index = find_index_from_env(env_list->env, cmd, 1);
+	cmd[j] = '=';
+	if (index < 0)
+	{
+		len = count_array_of_str(env_list->env);
+		tmp = array_dup(env_list->env, 1);
+		free(env_list->env);
+		env_list->env = tmp;
+		env_list->env[len] = ft_strdup(cmd);
+	}
+	else
+	{
+		free(env_list->env[index]);
+		env_list->env[index] = ft_strdup(cmd);
+	}
+}
+
+void add_in_export(char *cmd, t_export *env_list)
+{
+	int len;
+	char **tmp;
+
+	len = find_index_from_env(env_list->env, cmd, 1);
+	if (len < 0)
+		len = find_index_from_env(env_list->export, cmd, 2);
+	if (len >= 0)
+		return ;
+	len = count_array_of_str(env_list->export);
+	tmp = array_dup(env_list->export, 1);
+	free(env_list->export);
+	env_list->export = tmp;
+	env_list->export[len] = ft_strdup(cmd);
+}
+
+void add_in_variable(char *cmd, int j, t_export *env_list)
+{
+	char *tmp;
+	char **array;
+	int index;
+	
+	cmd[j] = '\0';
+	index = find_index_from_env(env_list->export, cmd, 2);
+	if (index < 0)
+	{
+		index = find_index_from_env(env_list->env, cmd, 1);
+		if(index >= 0)
+		{
+			tmp = ft_strjoin(env_list->env[index], cmd + j + 2);
+			free(env_list->env[index]);
+			env_list->env[index] = tmp;
+		}
+	}
+	else
+	{
+		array = array_remove(env_list->export, index);
+		free(env_list->export);
+		env_list->export = array;
+		index = -1;
+	}
+	if (index < 0)
+	{
+		tmp = ft_strjoin(cmd, cmd + j + 1);
+		array = array_dup(env_list->env, 1);
+		index = count_array_of_str(env_list->env);
+		free(env_list->env);
+		env_list->env = array;
+		env_list->env[index] = tmp;
+	}
+}
+
+int find_variable(char *cmd, t_export *env_list)
+{
+	int j;
+
+	j = 0;
+	while(cmd[j])
+	{
+		if (!((!j && ft_isalpha(cmd[j])) || (j && ft_isalnum(cmd[j]))))
+		{
+			if (cmd[j] == '=')
+				add_in_env(cmd, env_list, j);
+			else if (cmd[j] == '+' && !ft_strncmp(cmd + j, "+=", 2))
+				add_in_variable(cmd, j, env_list);
+			else
+			{
+				printf("export: `%s': not a valid identifier\n", cmd);
+				return(-1);
+			}
+			break;
+		}
+		j++;
+		if (!cmd[j])
+			add_in_export(cmd, env_list);
+	}
+	return (1);
+}
+
+void ft_export(char **cmd, t_export *env_list)
 {
 	char **tmp;
+	int i;
 
 	tmp = NULL;
 	if (count_array_of_str(cmd) == 1)
-		;
-	printf("hello world!\n");
+	{
+		ft_env(env_list->env);
+		ft_env(env_list->export);
+	}
+	else
+	{
+		i = 1;
+		while(cmd[i])
+		{
+			if (find_variable(cmd[i], env_list) < 0)
+				break;
+			i++;
+		}
+	}
 }
 
-void ft_unset(char **cmd, char **env)
-{
-	printf("fuck off\n");
-}
 
-int ft_execve(char **cmd, char **env)
+int ft_execve(char **cmd, t_export *env_list)
 {
-	if (cmd_quote_handler(cmd, env))
+	if (cmd_quote_handler(cmd, env_list->env))
 		return (1);
 	if (!cmd || !cmd[0])
 	    return (1);
@@ -181,19 +360,17 @@ int ft_execve(char **cmd, char **env)
 	else if (!ft_strcmp(cmd[0], "echo"))
 	    ft_echo(cmd);
 	else if(!ft_strcmp(cmd[0], "export"))
-	    env = ft_export(cmd, env);
+	    ft_export(cmd, env_list);
 	else if(!ft_strcmp(cmd[0], "unset"))
-	    ft_unset(cmd, env);
+	    ft_unset(cmd, env_list);
 	else if(!ft_strcmp(cmd[0], "exit"))
 	    exit(0);
 	else if (!ft_strcmp(cmd[0], "env"))
 	{
 		if (cmd[1])
-		{
             write(2, "env with options or arguments\n", 31);
-            exit (1);
-        }
-	    ft_env(env);
+		else
+	    	ft_env(env_list->env);
 	}
 	else
 		return (0);
@@ -233,7 +410,7 @@ int redirections(t_list *list)
 	return(0);
 }
 
-int command_line(t_list *list, int *fd, int fd_tmp, char **path, char **env)
+int command_line(t_list *list, int *fd, int fd_tmp, char **path, t_export *env_list)
 {
 	char *cmd_name;
 	char **cmd;
@@ -251,11 +428,11 @@ int command_line(t_list *list, int *fd, int fd_tmp, char **path, char **env)
 		dup2(fd[1], STDOUT_FILENO);
 		if (redirections(list) < 0)
 		    exit (1);
-	    if (ft_execve(cmd, env))
+	    if (ft_execve(cmd, env_list))
 			exit (0);
 		cmd_name = cmd_path(cmd[0], path);
 		if (cmd_name)
-			execve(cmd_name, cmd, env);
+			execve(cmd_name, cmd, env_list->env);
 		write(2, cmd[0], ft_strlen(cmd[0]));
 		ft_putstr_fd(": command not found\n", 2);
 		exit(127);
@@ -263,7 +440,7 @@ int command_line(t_list *list, int *fd, int fd_tmp, char **path, char **env)
 	return (0);
 }
 
-int last_command(t_list *list, int fd_tmp, char **path, char **env)
+int last_command(t_list *list, int fd_tmp, char **path, t_export *env_list)
 {
 	char *cmd_name;
 	char **cmd;
@@ -279,11 +456,11 @@ int last_command(t_list *list, int fd_tmp, char **path, char **env)
 			dup2(fd_tmp, STDIN_FILENO);
 		if (redirections(list))
 		    exit (1);
-	    if (((t_ogbi *)(list->content))->i && ft_execve(cmd, env))
+	    if (((t_ogbi *)(list->content))->i && ft_execve(cmd, env_list))
 			exit (0);
 		cmd_name = cmd_path(cmd[0], path);
 		if (cmd_name)
-			execve(cmd_name, cmd, env);
+			execve(cmd_name, cmd, env_list->env);
 		write(2, cmd[0], ft_strlen(cmd[0]));
 		ft_putstr_fd(": command not found\n", 2);
 		exit(127);
@@ -310,7 +487,7 @@ int is_buldin(char *cmd)
 	return (0);
 }
 
-int last_execve(t_list *list, char **env)
+int last_execve(t_list *list, t_export *env_list)
 {
 	int fd1;
 	int fd2;
@@ -326,7 +503,7 @@ int last_execve(t_list *list, char **env)
 		if (fd2 < 0)
 			return(error("dup"));
 		redirections(list);
-		((t_ogbi *)(list->content))->i = ft_execve(((t_ogbi *)(list->content))->cmd, env);
+		((t_ogbi *)(list->content))->i = ft_execve(((t_ogbi *)(list->content))->cmd, env_list);
 		fd1 = dup2(fd1, STDIN_FILENO);
 		if (fd1 < 0)
 			return(error("dup2"));
@@ -337,7 +514,7 @@ int last_execve(t_list *list, char **env)
 	return (0);
 }
 
-int	pipex(t_list *list, char **env)
+int	pipex(t_list *list, t_export *env_list)
 {
 	char **path;
 	int status;
@@ -345,7 +522,7 @@ int	pipex(t_list *list, char **env)
 	int i;
 	int fd[2];
 
-	path = ft_split(find_str_from_env(env, "PATH"), ':');
+	path = ft_split(find_str_from_env((char **)(env_list->env), "PATH"), ':');
 	fd_tmp = STDIN_FILENO;
 	i = 0;
 	((t_ogbi *)(list->content))->i = i;
@@ -356,7 +533,7 @@ int	pipex(t_list *list, char **env)
 			del(path);
 			return(error("pipe() failed"));
 		}
-		if (command_line(list, fd, fd_tmp, path, env))
+		if (command_line(list, fd, fd_tmp, path, env_list))
 		    return (del(path));
 		close(fd[1]);
 		if (fd_tmp)
@@ -365,7 +542,7 @@ int	pipex(t_list *list, char **env)
 		list = list->next;
 		((t_ogbi *)(list->content))->i = ++i;
 	}
-	if (last_execve(list, env) || (!((t_ogbi *)(list->content))->i && last_command(list, fd_tmp,path, env) == -1))
+	if (last_execve(list, env_list) || (!((t_ogbi *)(list->content))->i && last_command(list, fd_tmp,path, env_list) == -1))
 		return(del(path));
 	if (fd_tmp)
 		close(fd_tmp);
